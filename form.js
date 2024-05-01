4e3357c8f461a603154a7e2c051172a553e0d82a
56treskka <134858289+56treskka@users.noreply.github.com>
2024-05-01 19:32:02 +0400

    Add files via upload

---
+++ form.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector('.btn');
+
+function handle() {
+    let inputs = document.querySelectorAll("input");
+    let input_values = [];
+    for (let i = 0; i < inputs.length; i++) {
+        input_values.push(inputs[i].value);
+    }
+}
+
+document.querySelector('click', handle);
